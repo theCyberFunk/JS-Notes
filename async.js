fecfc9ff63c7449cd72befc330d420d8d3945ab1
Aditya <adityafav@gmail.com>
2022-12-23 17:25:10 +0530

    add : wednesday.is

--- async.js
+++ async.js
@@ -1,9 +1,9 @@
 //=== Controlling the Event Loop with Promises ==== //
 
-const fs = require("fs");
+import { readFile } from "fs";
 
 const file = new Promise((resolve, reject) => {
-  fs.readFile("text.md", "utf8", (err, data) => {
+  readFile("text.md", "utf8", (err, data) => {
     if (err) reject(err);
     resolve(data);
   });
@@ -30,10 +30,35 @@ delayedMessage.then(() => console.log("Anything"));
 // PAYTM INSIDER QUESTION //
 
 const delayedMessage1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(console.log("this is the first message"));
+  }, 1000);
+});
+
+delayedMessage1.then(() => console.log("Anything"));
+
+
+
+
+//  WEDNESDAY.IS QUESTION //
+
+async function main(str) {
+  //    reverse(capiatlize(str)).then(res => console.log(res));
+  let reversed = await reverse(str);
+  let capitalized = capitalize(reversed);
+  console.log(capitalized);
+}
+
+function capitalize(str) {
+  return str.toUpperCase();
+}
+
+async function reverse(str) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(console.log("this is the first message"));
-    }, 1000);
+      resolve(str.split("").reverse().join(""));
+    }, 2000);
   });
-  
-  delayedMessage1.then(() => console.log("Anything"));
-  
+}
+
+main("aditya");
--- dsa.js
+++ dsa.js
@@ -20,5 +20,24 @@ console.log(arr)
 toSort.sort(function (a, b) {
   return a - b;
 });
-
 // === Array sort === //
+
+
+
+
+// === Factorial through RECURSION === //
+function factorial(x) {
+  // if number is 0
+  if (x == 0) {
+    return 1;
+  }
+
+  // if number is positive
+  else {
+    console.log(x);
+    return x * factorial(x - 1);
+  }
+}
+
+console.log(factorial(4));
+// === Factorial through RECURSION === //
